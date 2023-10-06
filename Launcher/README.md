38009c42b4faa15ef27c54cb4cf8c05c4fb41afb
Hellfurian <Tyler.Long1228@gmail.com>
2023-10-06 18:25:59 -0400

    Add Launcher

---
+++ Launcher/README.md
@@ -0,0 +1,38 @@
+# Launcher
+
+Custom launcher for Escape From Tarkov to start the game in offline mode
+
+**Project**        | **Function**
+------------------ | --------------------------------------------
+Aki.Build          | Build script
+Aki.ByteBanger     | Assembly-CSharp.dll patcher
+Aki.Launcher       | Launcher frontend
+Aki.Launcher.Base  | Launcher backend
+
+## Privacy
+SPT is an open source project. Your commit credentials as author of a commit will be visible by anyone. Please make sure you understand this before submitting a PR.
+Feel free to use a "fake" username and email on your commits by using the following commands:
+```bash
+git config --local user.name "USERNAME"
+git config --local user.email "USERNAME@SOMETHING.com"
+```
+
+## Requirements
+
+- Escape From Tarkov 26215
+- .NET 6 SDK
+- Visual Studio Code
+
+### For UI Development
+
+- Visual Studio Community 2022 (.NET desktop workload)
+- Avalonia Visual Studio Extension
+
+## Build
+1. Run `dotnet tool restore` from command line inside project folder
+2. Open Launcher.code-workspace in Visual Studio Code.
+3. Run the build task: (top toolbar) Terminal -> Run Build Task...
+4. Copy-paste all files inside `Build` into `game root directory`, overwrite when prompted.
+
+## Server Endpoints
+If you just want to mess with the server endpoints, you can use this [postman collection](https://gofile.io/d/kCzmze)
---
+++ Launcher/project/Aki.Launcher.Base/Controllers/ServerManager.cs
@@ -0,0 +1,121 @@
+/* ServerManager.cs
+ * License: NCSA Open Source License
+ * 
+ * Copyright: Merijn Hendriks
+ * AUTHORS:
+ * Merijn Hendriks
+ */
+
+
+using Aki.Launcher.MiniCommon;
+using Aki.Launcher.Models.Aki;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Aki.Launcher
+{
+    public static class ServerManager
+    {
+        public static ServerInfo SelectedServer { get; private set; } = null;
+
+        public static bool PingServer()
+        {
+            string json = "";
+
+            try
+            {
+                json = RequestHandler.SendPing();
+
+                if(json != null) return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public static string GetVersion()
+        {
+            try
+            {
+                string json = RequestHandler.RequestServerVersion();
+
+                return Json.Deserialize<string>(json);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static string GetCompatibleGameVersion()
+        {
+            try
+            {
+                string json = RequestHandler.RequestCompatibleGameVersion();
+
+                return Json.Deserialize<string>(json);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static Dictionary<string, AkiServerModInfo> GetLoadedServerMods()
+        {
+            try
+            {
+                string json = RequestHandler.RequestLoadedServerMods();
+
+                return Json.Deserialize<Dictionary<string, AkiServerModInfo>>(json);
+            }
+            catch
+            {
+                return new Dictionary<string, AkiServerModInfo>();
+            }
+        }
+
+        public static AkiProfileModInfo[] GetProfileMods()
+        {
+            try
+            {
+                string json = RequestHandler.RequestProfileMods();
+
+                return Json.Deserialize<AkiProfileModInfo[]>(json);
+            }
+            catch
+            {
+                return new AkiProfileModInfo[] { };
+            }
+        }
+
+        public static void LoadServer(string backendUrl)
+        {
+            string json = "";
+
+            try
+            {
+                RequestHandler.ChangeBackendUrl(backendUrl);
+                json = RequestHandler.RequestConnect();
+            }
+            catch
+            {
+                SelectedServer = null;
+                return;
+            }
+
+            SelectedServer = Json.Deserialize<ServerInfo>(json);
+        }
+
+        public static async Task LoadDefaultServerAsync(string server)
+        {
+            await Task.Run(() =>
+            {
+                LoadServer(server);
+            });
+        }
+    }
+}
